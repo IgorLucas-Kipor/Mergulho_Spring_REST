b3877edc919c8b747938d1c9006aa435e8e04397
Igor Lucas <igorescritor@hotmail.com>
2021-05-21 15:38:15 -0300

    Commit inicial - começando o projeto

---
+++ igorlog-api/src/main/java/com/algaworks/igorlog/IgorlogApiApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.igorlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgorlogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IgorlogApiApplication.class, args);
+	}
+
+}
---
+++ igorlog-api/src/test/java/com/algaworks/igorlog/IgorlogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algaworks.igorlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IgorlogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
