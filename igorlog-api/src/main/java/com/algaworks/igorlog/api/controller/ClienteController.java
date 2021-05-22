a995f4092e6fab4b361c055ff4fb00725b22298d
Igor Lucas <igorescritor@hotmail.com>
2021-05-22 12:39:43 -0300

    Implementando o Jakarta Persistence

--- igorlog-api/src/main/java/com/algaworks/igorlog/api/controller/ClienteController.java
+++ igorlog-api/src/main/java/com/algaworks/igorlog/api/controller/ClienteController.java
@@ -1,7 +1,9 @@
 package com.algaworks.igorlog.api.controller;
 
-import java.util.Arrays;
 import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,15 +12,16 @@ import com.algaworks.igorlog.domain.model.Cliente;
 
 @RestController
 public class ClienteController {
+	
+	@PersistenceContext
+	private EntityManager manager;
 
 	@GetMapping("/clientes")
-	public Cliente listar() {
-		Cliente cliente1 = new Cliente();
-		cliente1.setId(7L);
-		cliente1.setNome("Gabriel");
-		cliente1.setTelefone("34 74185-2156");
-		cliente1.setEmail("bielzinhovilas@gmail.com");
-		return cliente1;
+	public List<Cliente> listar() {
+		return manager.createQuery("from Cliente", Cliente.class)
+				.getResultList();
 	}
+	
+	//"from Cliente" = me dê todos os objetos da entidade Cliente
 
 }
