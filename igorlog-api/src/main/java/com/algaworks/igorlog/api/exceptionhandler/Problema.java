4ff7e0d228596247f862de4ebda0863873d47f0a
Igor Lucas <igorescritor@hotmail.com>
2021-05-23 03:04:35 -0300

    Implementando API Exception Handler

---
+++ igorlog-api/src/main/java/com/algaworks/igorlog/api/exceptionhandler/Problema.java
@@ -0,0 +1,25 @@
+package com.algaworks.igorlog.api.exceptionhandler;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Problema {
+	
+	private Integer status;
+	private LocalDateTime dataHora;
+	private String título;
+	private List<Campo> campos;
+	
+	@AllArgsConstructor
+	@Getter
+	public static class Campo {
+		private String nome;
+		private String mensagem;
+	}
+}
